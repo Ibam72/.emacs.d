a9f699001b08375c9f3a62d36f7f318c140d4034
karen-kamishiro <karen-kamishiro@v0047.geniee.jp>
2019-05-03 08:45:04 +0900

    fix:phpは使わない

--- init.el
+++ init.el
@@ -1,4 +1,6 @@
-;; Language
+;;; package --- Sammury
+;;; Commentary:
+;;; Code:
 (set-language-environment 'Japanese)
 
 ;; Coding system
@@ -18,18 +20,16 @@
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 
 (package-initialize)
-;;(package-refresh-contents)
+;(package-refresh-contents)
 
-
-(package-install 'ac-php)
+;;(package-install 'ac-php)
 (package-install 'irony)
 (package-install 'kotlin-mode)
 ;;(package-install 'ac-html-bootstrap)
 (package-install 'twittering-mode)
 (package-install 'sr-speedbar)
 (package-install 'use-package)
-
-
+(package-install 'company-irony)
 (package-install 'hiwin)
 (package-install 'linum)
 (package-install 'elscreen)
@@ -47,31 +47,22 @@
 (package-install 'tree-mode)
 (package-install 'elmacro)
 (package-install 'magit)
+(package-install 'go-mode)
+(package-install 'company-go)
 (global-flycheck-mode)
 
-(add-to-list 'exec-path "C:/Program Files/LLVM/bin") ;;windows
 (require 'use-package)
 (require 'twittering-mode)
 (require 'elmacro)
 (elmacro-mode)
 (require 'magit)
 
-;; init load
-;;(require 'init-loader)
-;;(setq init-loader-show-log-after-init nil)
-;;(init-loader-load "~/.emacs.d/inits/")
-
-;; emacs style
-;;(add-to-list 'default-frame-alist '(font . "ricty-12")) ;mac
-;;(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-;;(setq custom-theme-directory "~/.emacs.d/themes")
 (require 'hiwin)
 (hiwin-activate)
 (set-face-background 'hiwin-face "#ffeeff")
 (require 'linum)
 (global-linum-mode 1)
-(setq default-tab-width 4)
-; elscreen
+(setq-default tab-width 4 indent-tabs-mode nil)
 (require 'elscreen)
 (elscreen-start)
 (global-set-key (kbd "s-t") 'elscreen-create)
@@ -90,16 +81,8 @@
 (set-face-attribute 'elscreen-tab-other-screen-face nil
                     :background "grey30"
                     :foreground "grey60")
-;; neotree
 (require 'neotree)
 (global-set-key "\C-o" 'neotree-toggle)
-
-;; macro
-;;(let frame-parameter buffer-list)
-;;(let frame-selected-window )
-;;(fset 'kill-buffer-and-frame
-;;	  [() ?\S-\C-e ?\M-w ?\C-e ?\C-j ?\C-a ?\S-\C-e ?\C-y ?\C-a])
-;; Keys
 
 (global-set-key (kbd "C-x g") 'magit-status)
 
@@ -119,29 +102,13 @@
 ;;(global-set-key (kbd "C-<f8>") ')
 ;;(global-set-key (kbd "M-<f8>") ')
 
-;;(global-set-key (kbd "<f9>") 'eshell)
+(global-set-key (kbd "<f9>") 'replace-string)
 ;;(global-set-key (kbd "C-<f9>") ')
 ;;(global-set-key (kbd "M-<f9>") ')
 
 (global-set-key (kbd "<f10>") 'quickrun)
 (global-set-key (kbd "C-<f10>") 'quickrun-with-arg)
 (global-set-key (kbd "M-<f10>") 'quickrun-compile-only)
-
-
-;;(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
-;; '(package-selected-packages
-;;   (quote
-;;	(twitering-mode tree-mode company-web company-axiom magit ac-html-bootstrap kotlin-mode elmacro sr-speedbar flycheck-irony init-loader quickrun ac-php yaml-mode neotree elscreen linum hiwin el-get use-package markdown-mode emmet-mode web-mode php-mode js2-mode flycheck company-irony irony company))))
-;;(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- ;;)
 
 (require 'sr-speedbar)
 (setq sr-speedbar-right-side nil)
@@ -152,9 +119,12 @@
 (setq company-idle-delay 0)
 (setq company-minimum-prefix-length 4)
 (setq company-selection-wrap-around t)
-(setq w32-pipe-read-delay 0) ; for windows
 
-
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (c-set-style "linux")
+            (setq indent-tabs-mode nil)
+            (setq c-basic-offset 4)))
 ;; C,C++
 (require 'irony)
 (add-hook 'c-mode-hook 'irony-mode)
@@ -164,23 +134,29 @@
 (add-to-list 'company-backends 'company-irony)
 (eval-after-load 'flycheck
   '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
+(require 'flycheck)
+;(add-hook 'c++-mode-hook
+;          (lambda () (setq flycheck-clang-include-path
+;                           (list (expand-file-name "~/work/gitlab/galaxy/src/")))))
 
 ;; C,C++ compile option
 (setq irony-lang-compile-option-alist
-      '((c++-mode . ("c++" "-std=c++17" "-lstdc++" "-lm"))
-        (c-mode . ("c"))
-        (objc-mode . '("objective-c"))))
-(defun irony--lang-compile-option ()
-  (irony--awhen (cdr-safe (assq major-mode irony-lang-compile-option-alist))
-    (append '("-x") it)))
+      (quote ((c++-mode . "c++ -std=c++17 -lstdc++")
+              (c-mode . "c")
+              (objc-mode . "objective-c"))))
+(defun ad-irony--lang-compile-option ()
+  (defvar irony-lang-compile-option-alist)
+  (let ((it (cdr-safe (assq major-mode irony-lang-compile-option-alist))))
+    (when it (append '("-x") (split-string it "\s")))))
+(advice-add 'irony--lang-compile-option :override #'ad-irony--lang-compile-option)
 
-
- ;; tern && js2-mode 4 javasprict
+;; tern && js2-mode 4 javasprict
+(require 'tern)
 (require 'js2-mode)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-(setq company-tern-property-marker "")
+;(setq company-tern-property-marker "")
 (defun company-tern-depth (candidate)
-  "Return depth attribute for CANDIDATE. 'nil' entries are treated as 0."
+  "Return depth attribute for CANDIDATE.'nil' entries are treated as 0."
   (let ((depth (get-text-property 0 'depth candidate)))
     (if (eq depth nil) 0 depth)))
 (add-hook 'js2-mode-hook 'tern-mode)
@@ -195,17 +171,16 @@
 ;; comannd "touch .ac-php-conf.json"
 ;; and     " M-x ac-php-remake-tags-all
 ;; in top derectory
-(require 'cl)
-(require 'php-mode)
-(add-hook 'php-mode-hook
-            '(lambda ()
-               (auto-complete-mode t)
-               (require 'ac-php)
-               (setq ac-sources  '(ac-source-php ) )
-               (yas-global-mode 1)
-               (define-key php-mode-map  (kbd "C-]") 'ac-php-find-symbol-at-point)   ;goto define
-               (define-key php-mode-map  (kbd "C-t") 'ac-php-location-stack-back   ) ;go back
-               ))
+;;(require 'cl)
+;;(require 'php-mode)
+;;(add-hook 'php-mode-hook
+;;            '(lambda ()
+;;               (auto-complete-mode t)
+;;               (setq ac-sources  '(ac-source-php ) )
+;;               (yas-global-mode 1)
+;;               (define-key php-mode-map  (kbd "C-]") 'ac-php-find-symbol-at-point)   ;goto define
+;;               (define-key php-mode-map  (kbd "C-t") 'ac-php-location-stack-back   ) ;go back
+;;               ))
 
 
 
@@ -218,8 +193,10 @@
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(setq web-mode-engines-alist
-'(("php"    . "\\.phtml\\'")
+(setq web-mode-markup-indent-offset 2)
+
+;(setq web-mode-engines-alist
+;'(("php"    . "\\.phtml\\'")
 (use-package markdown-mode
   :ensure t
   :commands (markdown-mode gfm-mode)
@@ -227,3 +204,53 @@
          ("\\.md\\'" . markdown-mode)
          ("\\.markdown\\'" . markdown-mode))
   :init (setq markdown-command "multimarkdown"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(magit-diff-arguments nil)
+ '(package-selected-packages
+   (quote
+    (go-mode company-go flycheck php-mode company linum irony yaml-mode web-mode use-package twittering-mode tree-mode sr-speedbar quickrun neotree markdown-mode magit kotlin-mode js2-mode init-loader hiwin flycheck-irony emmet-mode elscreen elmacro company-web company-tern company-irony-c-headers company-irony ac-php))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Goのパスを通す
+(add-to-list 'exec-path (expand-file-name "/usr/local/bin/go"))
+;; go get で入れたツールのパスを通す
+(add-to-list 'exec-path (expand-file-name "/Users/karen-kamishiro/dev/bin"))
+
+;; flycheck-modeを有効化してシンタックスエラーを検知
+(add-hook 'go-mode-hook 'flycheck-mode)
+(add-hook 'go-mode-hook (lambda()
+       (add-hook 'before-save-hook' 'gofmt-before-save)
+       (local-set-key (kbd "M-.") 'godef-jump)
+       (set (make-local-variable 'company-backends) '(company-go))
+       (setq indent-tabs-mode nil)    ; タブを利用
+       (setq c-basic-offset 4)    ; tabサイズを4にする
+       (setq tab-width 4)))
+
+;; company-modeとの連携してコード補完する
+(require 'company-go)
+(add-hook 'go-mode-hook (lambda()
+      (company-mode)
+      (setq company-transformers '(company-sort-by-backend-importance)) ;; ソート順
+      (setq company-idle-delay 0) ; 遅延なしにすぐ表示
+      (setq company-minimum-prefix-length 3) ; デフォルトは4
+      (setq company-selection-wrap-around t) ; 候補の最後の次は先頭に戻る
+      (setq completion-ignore-case t)
+      (setq company-dabbrev-downcase nil)
+      (global-set-key (kbd "C-M-i") 'company-complete)
+      ;; C-n, C-pで補完候補を次/前の候補を選択
+      (define-key company-active-map (kbd "C-n") 'company-select-next)
+      (define-key company-active-map (kbd "C-p") 'company-select-previous)
+      (define-key company-active-map (kbd "C-s") 'company-filter-candidates) ;; C-sで絞り込む
+      (define-key company-active-map [tab] 'company-complete-selection) ;; TABで候補を設定
+      (define-key emacs-lisp-mode-map (kbd "C-M-i") 'company-complete) ;; 各種メジャーモードでも C-M-iで company-modeの補完を使う
+      ))
+
